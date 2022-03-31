4d886eade2e97310baf630813a1d0b337201d19a
MaureenEk <mekweribe28@gmail.com>
2022-03-30 20:23:50 -0400

    a new assignment

---
+++ Object.js
@@ -0,0 +1,14 @@
+let favoriteBand = {
+    name: 'westlife',
+    country: 'ireland',
+    genre: 'POP',
+    number: 4,
+    year: 1998,
+    split: false,
+    album: [
+        "greatest hits, 2011", "world of our own, 2001","westlife, 1999",
+        "spectrum 2019", "where we are 2009", "back home 2007",
+        "coast to coast 2000", "wilddreams 2021"
+    ]
+ 
+}
